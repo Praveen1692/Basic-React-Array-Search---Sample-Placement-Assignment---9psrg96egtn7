449a7f4b3fe7dcd54106ad63447c74a9bd9882b0
Praveen1692 <61341264+Praveen1692@users.noreply.github.com>
2022-10-05 09:14:48 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -5,10 +5,45 @@ import '../styles/App.css';
 
 const searchArray = ["Newton", "School", "Newton School", "React", "Preact", "Node", "Mongoose", "Angular", "Vue", "Apple", "Microsoft", "Netflix", "Meta"]
 const App = () => {
+  
+  
+   const[searchTerm,setSearchTerm]=useState('');
+
+
+  const updatedList=searchArray.filter((val)=>{
+    //if(searchTerm==''){
+     // return val=="";
+    if(val.toLocaleLowerCase().includes(searchTerm.toLocaleLowerCase())){
+      return <li key={val.toString()}>{val}</li>
+    }
+    
+  })
 
   
   return (
     <div id="main">
+    
+    <input type="text" placeholder='search...' onChange={event=>{setSearchTerm(event.target.value)}} />
+
+        <h1>Result:--*--*++*-----</h1>
+
+
+
+
+       { /*  } {searchArray.filter((val)=>{
+          if(searchTerm==""){
+            return val=="";
+          }else if(val.toLocaleLowerCase().includes(searchTerm.toLowerCase())){
+            return <li> {val}</li>
+
+          }
+        })}
+      c*/}
+
+     
+
+
+      <ul>{updatedList}</ul>
       
     </div>
   )
